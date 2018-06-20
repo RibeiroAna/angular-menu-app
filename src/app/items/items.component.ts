81e44ff68d826579f7f1796adfe65990bf338cd0
Ana Ribeiro <aninhacostaribeiro@gmail.com>
2018-06-19 22:40:20 -0300

    Step 1 - added menu of items

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from './item.interface';
 
 @Component({
   selector: 'app-items',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-  items = ['pizza', 'soda'];
+  items: Item [] = [{
+    name: 'Pizza',
+    price: 3
+  },
+  {
+    name: 'Salad',
+    price: 2
+  }];
   constructor() { }
 
   ngOnInit() {
